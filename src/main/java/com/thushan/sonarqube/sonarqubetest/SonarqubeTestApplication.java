e947a177c0e423181ea12b996145d80cdc3c75f7
ThushanKavishka <thushankavishka@gmail.com>
2022-02-08 23:07:48 +0530

    Initial commit

---
+++ src/main/java/com/thushan/sonarqube/sonarqubetest/SonarqubeTestApplication.java
@@ -0,0 +1,13 @@
+package com.thushan.sonarqube.sonarqubetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarqubeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thushan/sonarqube/sonarqubetest/SonarqubeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thushan.sonarqube.sonarqubetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
